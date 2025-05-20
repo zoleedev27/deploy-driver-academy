811d6ef6dfe90ce99125ea3392d566bc09c01037
Felhazi Zoltan <zoltan.felhazi@lynxsolutions.eu>
2025-05-20 11:39:32 +0300

    fix: navs

--- src/interfaces/Navs.ts
+++ src/interfaces/Navs.ts
@@ -1,16 +1,16 @@
 import {
-    Home,
-    BookOpen,
-    Newspaper,
-    Image,
-    Mail,
-    LucideIcon
+  Home,
+  BookOpen,
+  Newspaper,
+  Image,
+  Mail,
+  LucideIcon,
 } from "lucide-react";
 
 export const IconMap: Record<string, LucideIcon> = {
-    "Home": Home,
-    "Courses": BookOpen,
-    "Blog": Newspaper,
-    "Gallery": Image,
-    "Contact": Mail
+  Home: Home,
+  Courses: BookOpen,
+  Blog: Newspaper,
+  Gallery: Image,
+  Contact: Mail,
 };
